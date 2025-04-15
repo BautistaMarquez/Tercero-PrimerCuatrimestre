edcba243f6680059187caebf922eb0c80a833c29
BautistaMarquez <115323481+BautistaMarquez@users.noreply.github.com>
2025-04-15 20:09:58 -0300

    ClaseDeConsulta 15/4

--- Objetos 2/Resoluciones Practicas OO2/OO2.Refactoring/src/main/java/Patrones/ej-biblioteca/src/main/java/Ejercicio5/CriterioDeSugerencia.java
+++ Objetos 2/Resoluciones Practicas OO2/OO2.Refactoring/src/main/java/Patrones/ej-biblioteca/src/main/java/Ejercicio5/CriterioDeSugerencia.java
@@ -2,6 +2,7 @@ package Ejercicio5;
 
 import java.util.List;
 //En estos casos no estoy teniendo a cuenta que no llegue a 3 peliculas
+//Para el tema de la similaridad tener un set en la clase para que una pelicula reciba la lista de peliculas que el usuario vio y este se fije si es similar a alguna de todas esas.
 public interface CriterioDeSugerencia {
 	public List<Pelicula> sugerir(Decodificador deco);
 };
